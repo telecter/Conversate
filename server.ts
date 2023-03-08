566f443f6f893f87bceb5396ad47d68516ee1796
Alexander Cidon <alexcidon9@gmail.com>
2023-03-07 21:34:13 -0500

    Modified server.ts

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
 import { serve } from "https://deno.land/std@0.136.0/http/server.ts";
 import { fetchMessages } from "./client.ts";
 
-serve((req: Request) => new Response(fetchMessages()));
+serve((req: Request) => new Response("AAAA"));
