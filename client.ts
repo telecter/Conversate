f6dbfd5d364827bae13b3cb326565dd0bbcc21f9
Alexander Cidon <alexcidon9@gmail.com>
2023-03-07 20:39:25 -0500

    Modified client.ts

--- client.ts
+++ client.ts
@@ -1,4 +1,4 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+import {createClient} from 'https://esm.sh/@supabase/supabase-js@2';
 
 const supabaseUrl: string = "chjbmduxkzytmcktgzvl.supabase.co";
 const supabase: any = createClient(supabaseUrl, Deno.env.get("SUPABASE_ANON_KEY"));
